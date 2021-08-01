e1d329d40a1c5f1bf34710c5f1de2d34b207f1cc
JIMHWANG <jimhwang1129@gmail.com>
2021-08-01 17:22:02 +0800

    Update 2021-08-01T09:21:58.482Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,5 +3,9 @@
 console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
+  $('.search').click(function (e) {
+    e.preventDefault();
+    $('.admin-search-input').toggleClass('active');
+  });
 });
 //# sourceMappingURL=all.js.map
