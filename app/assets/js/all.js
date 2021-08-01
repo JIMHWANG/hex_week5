17876064538ead0d55cb0fa67db59e3b533a4c0d
JIMHWANG <jimhwang1129@gmail.com>
2021-08-01 17:21:35 +0800

    admin header searchimg function

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,4 +2,8 @@ console.log('Hello!');
 
 $(document).ready(() => {
   console.log('HesSchool Hello!');
+  $('.search').click(function (e) {
+    e.preventDefault();
+    $('.admin-search-input').toggleClass('active');
+  })
 });
